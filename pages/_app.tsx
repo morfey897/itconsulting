84f544c16714becafad9ee2186055b2802e629ea
Maksym Cherniavskyi <morfey897@gmail.com>
2022-12-13 14:02:26 +0200

    Set up

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+
+import React from 'react';
+
+import { Roboto } from '@next/font/google';
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+})
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style> */}
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </>
+  )
+}
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import style from "./index.module.scss";
+
+function HomePage() {
+  return <div className={style.text}>Welcome to Next.js!</div>
+}
+
+export default HomePage;
