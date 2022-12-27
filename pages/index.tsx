5a1eb9e357b4c3022c6c9ec7ddceafb66c70f52d
Maksym Cherniavskyi <morfey897@gmail.com>
2022-12-27 23:05:48 +0200

    Edge runtime

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,21 +5,14 @@ import style from "./index.module.scss";
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
-  // const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-  // const data = await res.json();
-
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  const data = await res.json();
   // Pass data to the page via props
-  return { props: { data: {
-    someJson: 'str',
-    test: 1,
-    getServerSideProps: 'getServerSideProps',
-
-  } } }
+  return { props: { data } }
 }
 
-function HomePage(props) {
-  console.log('PROPS', props);
-  return <div className={style.text}>Welcome to Next.js!</div>
+function HomePage({ data }) {
+  return <div className={style.text}>Welcome to Next.js!: {JSON.stringify(data)}</div>
 }
 
 export default HomePage;
