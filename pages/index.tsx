624764bbe4bf07030d29b07adc31c795ea52e83b
Maksym Cherniavskyi <morfey897@gmail.com>
2022-12-27 22:38:01 +0200

    Test ServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,19 @@
 import React from "react"
 import style from "./index.module.scss";
 
-function HomePage() {
-  return <div className={style.text}>Welcome to Next.js!</div>
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  const data = await res.json()
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
+function HomePage({ data }) {
+  return <div className={style.text}>Welcome to Next.js!: {JSON.stringify(data)}</div>
 }
 
 export default HomePage;
