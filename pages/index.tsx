52cf8d1009edb1c2670c1cef9c5e726e47ff48d5
Maksym Cherniavskyi <morfey897@gmail.com>
2022-12-27 23:18:36 +0200

    Changed index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,22 @@
 import React from "react"
 import style from "./index.module.scss";
 
-
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-  const data = await res.json();
+  // const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  // const data = await res.json();
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data: {
+    some: 'some',
+    data: 1,
+    time: '12:23',
+    has: false,
+  } } }
 }
 
 function HomePage({ data }) {
-  return <div className={style.text}>Welcome to Next.js!: {JSON.stringify(data)}</div>
+  return <div className={style.text}>Welcome to Next.js!: {JSON.stringify(data, undefined, 2)}</div>
 }
 
 export default HomePage;
