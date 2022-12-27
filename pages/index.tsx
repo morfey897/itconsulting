651b194759e0199b9930bce64c46a5cda4a219f4
Maksym Cherniavskyi <morfey897@gmail.com>
2022-12-27 22:53:25 +0200

    Test deploy

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,16 @@ import style from "./index.module.scss";
 // This gets called on every request
 export async function getServerSideProps() {
   // Fetch data from external API
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
-  const data = await res.json()
+  // const res = await fetch('https://pokeapi.co/api/v2/pokemon/ditto')
+  // const data = await res.json();
 
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data: {
+    someJson: 'str',
+    test: 1,
+    getServerSideProps: 'getServerSideProps',
+    
+  } } }
 }
 
 function HomePage({ data }) {
