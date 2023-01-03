8f0353feae67b7fc7379cc92b1dcf7ff996ad20d
yury <yury.inform@gmail.com>
2023-01-03 11:26:27 +0200

    test commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,8 @@ import "../styles/globals.css";
 export default function MyApp({ Component, pageProps }) {
   return (
     <main className=''>
+
+        <div>123</div>
       <Component {...pageProps} />
     </main>
   )
