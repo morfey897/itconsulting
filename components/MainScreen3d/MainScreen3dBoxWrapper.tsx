146d82ab35e985f71456d0e73a1e5ee908a1dfd7
yury <yury.inform@gmail.com>
2023-05-16 11:33:00 +0300

    box automotion

--- components/MainScreen3d/MainScreen3dBoxWrapper.tsx
+++ components/MainScreen3d/MainScreen3dBoxWrapper.tsx
@@ -1,132 +1,114 @@
-import React, { useEffect } from 'react';
+import React, { useRef, useState } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { Mesh } from 'three';
 
-type MainScreen3dAnimationType = {
+type MainScreen3dBoxType = {
   width: number;
   height: number;
+  shiftsPointer: { x: number; y: number }[];
+  emitter: any;
 };
 
-function MainScreen3dAnimation({ width, height }: MainScreen3dAnimationType) {
-  useEffect(() => {
-    console.log(width, height);
-  }, [width, height]);
+type MeshRef = React.MutableRefObject<
+  Mesh & { rotation: { x: number; y: number } }
+>;
+
+function MainScreen3dBoxWrapper({
+  width,
+  height,
+  shiftsPointer,
+  emitter,
+}: MainScreen3dBoxType) {
+  const boxRef = useRef<MeshRef['current']>();
+  const [direction, setDirection] = useState(1);
+
+  const rotateProperties = {
+    x: {
+      default: 1,
+      limit: 0.5,
+      koefficient: 400,
+    },
+    y: {
+      default: 1,
+      limit: 0.5,
+      koefficient: 300,
+    },
+  };
+
+  useFrame(() => {
+    const automotion = shiftsPointer.length === 0;
+
+    let currentX = boxRef.current.rotation.x;
+    let currentY = boxRef.current.rotation.y;
+
+    let newX = null;
+    let newY = null;
+
+    if (automotion) {
+      newY =
+        currentY +
+        (rotateProperties.y.default / rotateProperties.y.koefficient) *
+          direction;
+
+      if (newY > rotateProperties.y.limit || newY < -rotateProperties.y.limit) {
+        setDirection((current) => -current);
+      }
+
+      if (currentX !== 0) {
+        let shiftX = currentX / (rotateProperties.x.koefficient / 20);
+
+        if (currentX < 0) {
+          if (currentX - shiftX < 0) {
+            newX = currentX - shiftX;
+          } else {
+            newX = 0;
+          }
+        } else {
+          if (currentX + shiftX > 0) {
+            newX = currentX - shiftX;
+          } else {
+            newX = 0;
+          }
+        }
+      }
+
+      boxRef.current.rotation.x = newX;
+      boxRef.current.rotation.y = newY;
+      return;
+    }
+
+    // It is not error, axes in three.js are inverted
+    let shiftX = shiftsPointer[0].y / rotateProperties.x.koefficient;
+    let shiftY = shiftsPointer[0].x / rotateProperties.y.koefficient;
+
+    newX = currentX + shiftX;
+
+    if (newX > rotateProperties.x.limit) {
+      newX = rotateProperties.x.limit;
+    } else if (newX < -rotateProperties.x.limit) {
+      newX = -rotateProperties.x.limit;
+    }
+
+    newY = currentY + shiftY;
+
+    if (newY > rotateProperties.y.limit) {
+      newY = rotateProperties.y.limit;
+    } else if (newY < -rotateProperties.y.limit) {
+      newY = -rotateProperties.y.limit;
+    }
+
+    boxRef.current.rotation.x = newX;
+    boxRef.current.rotation.y = newY;
+
+    emitter();
+  });
 
   return (
-    <mesh rotation={[0, 0, 0]} position={[0, 0, -height / 2]}>
-      <boxGeometry attach='geometry' args={[width, height, height]} />
-      <meshBasicMaterial attach='material' color='#6be092' />
+    <mesh ref={boxRef} rotation={[0, 0, 0]} position={[0, 0, -width / 2]}>
+      <boxGeometry attach='geometry' args={[width, height, width]} />
+      <meshPhysicalMaterial attach='material' color='#6be092' />
     </mesh>
   );
 }
 
-export default MainScreen3dAnimation;
-//
-// const [fov, setFov] = useState(60);
-// const [near, setNear] = useState(.1);
-// const [far, setFar] = useState(1000);
-// const [rotationX, setRotationX] = useState(0);
-// const [rotationY, setRotationY] = useState(0);
-// const [rotationZ, setRotationZ] = useState(0);
-// const [positionX, setPositionX] = useState(0);
-// const [positionY, setPositionY] = useState(0);
-// const [positionZ, setPositionZ] = useState(590);
-
-// <form>
-//   <RangeInput
-//     title={"fov"}
-//     id={"fov"}
-//     value={fov}
-//     changeFunction={setFov}
-//     min={0}
-//     max={360}
-//   />
-//   <RangeInput
-//     title={"near"}
-//     id={"near"}
-//     value={near}
-//     changeFunction={setNear}
-//     min={0.1}
-//     max={5}
-//     step={0.1}
-//   />
-//   <RangeInput
-//     title={"far"}
-//     id={"far"}
-//     value={far}
-//     changeFunction={setFar}
-//     min={1}
-//     max={2000}
-//   />
-//   <RangeInput
-//     title={"rotationX"}
-//     id={"rotationX"}
-//     value={rotationX}
-//     changeFunction={setRotationX}
-//     min={0}
-//     max={360}
-//   />
-//   <RangeInput
-//     title={"rotationY"}
-//     id={"rotationY"}
-//     value={rotationY}
-//     changeFunction={setRotationY}
-//     min={0}
-//     max={360}
-//   />
-//   <RangeInput
-//     title={"rotationZ"}
-//     id={"rotationZ"}
-//     value={rotationZ}
-//     changeFunction={setRotationZ}
-//     min={0}
-//     max={360}
-//   />
-//   <RangeInput
-//     title={"positionX"}
-//     id={"positionX"}
-//     value={positionX}
-//     changeFunction={setPositionX}
-//     min={0}
-//     max={360}
-//   />
-//   <RangeInput
-//     title={"positionY"}
-//     id={"positionY"}
-//     value={positionY}
-//     changeFunction={setPositionY}
-//     min={0}
-//     max={360}
-//   />
-//   <RangeInput
-//     title={"positionZ"}
-//     id={"positionZ"}
-//     value={positionZ}
-//     changeFunction={setPositionZ}
-//     min={0}
-{
-  /*    max={2000}*/
-}
-{
-  /*  />*/
-}
-{
-  /*</form>*/
-}
-
-//
-// function RangeInput({ title, id, value, changeFunction, min, max, step = 1 }) {
-//
-//   return (
-//     <label>
-//       {title}:
-//       <input
-//         id={id}
-//         type="range"
-//         value={value}
-//         min={min}
-//         max={max}
-//         step={step}
-//         onChange={(event) => changeFunction(event.target.value)} // Give them some love to grow!
-//       />
-//     </label>
-//   );
-// }
+export default MainScreen3dBoxWrapper;
--- components/MainScreen3d/MainScreen3dCanvas.tsx
+++ components/MainScreen3d/MainScreen3dCanvas.tsx
@@ -3,32 +3,74 @@ import { PerspectiveCamera } from '@react-three/drei';
 import { useElementSize } from '../../utilites/useElementSize';
 import MainScreen3dBoxWrapper from './MainScreen3dBoxWrapper';
 import style from './MainScreen3d.module.scss';
+import { useState } from 'react';
 
 const classNamePrefix = 'mainScreen3d';
 
 export default function MainScreen3dCanvas() {
   const [boxRef, { width, height }] = useElementSize();
+  const [positionPointer, setPositionPointer] = useState({ x: -1, y: -1 });
+  const [shiftsPointer, setShiftPointer] = useState([]);
+
+  // Handler pointer move
+  const handlerPointerMove = (event) => {
+    const { clientX: x, clientY: y } = event;
+
+    // Set initial position, if it is first mouse move
+    if (positionPointer.x === -1 || positionPointer.y === -1) {
+      setPositionPointer({ x, y });
+      return;
+    }
+
+    // Set shift pointer
+    setShiftPointer((currents) => {
+      return [
+        ...currents,
+        {
+          x: x - positionPointer.x,
+          y: y - positionPointer.y,
+        },
+      ];
+    });
+
+    // Set actual pointer position
+    setPositionPointer({ x, y });
+  };
+
+  // Remove handled pointer position
+  const removeHandledPointerPosition = () => {
+    setShiftPointer((current) => {
+      current.shift();
+
+      return current;
+    });
+  };
 
   return (
     <div>
       <div className={style[classNamePrefix]} ref={boxRef}>
-        <Canvas
-        // onPointerMove={(event) => {
-        //   // console.log(event.screenX);
-        //   // console.log(event.screenY);
-        //   // hover(true);
-        // }}
-        >
-          <MainScreen3dBoxWrapper width={width} height={height} />
+        <Canvas onPointerMove={handlerPointerMove}>
+          <MainScreen3dBoxWrapper
+            width={width}
+            height={height}
+            shiftsPointer={shiftsPointer}
+            emitter={removeHandledPointerPosition}
+          />
 
           <PerspectiveCamera
             makeDefault
             fov={60}
             aspect={width / height}
             near={0.1}
-            far={1000}
-            position={[0, 0, Math.round((Math.sqrt(3) / 2) * height) + 10]}
+            far={4000}
+            position={[
+              0,
+              0,
+              Math.round((Math.sqrt(3) / 2) * height) + 10 + 800,
+            ]}
           ></PerspectiveCamera>
+
+          <pointLight position={[500, 0, 2000]} />
         </Canvas>
       </div>
     </div>
